405dabfb27f3af9d52bcb846ffade1beb005be82
Suhas <suhaspr01@gmail.com>
2018-06-20 10:48:26 +0530

    Update FileEncryption.js

--- www/FileEncryption.js
+++ www/FileEncryption.js
@@ -11,22 +11,12 @@ var PLUGIN_NAME = 'FileEncryption';
 
 var FileEncryption = {
   encrypt: function (path, password, success, error) {
-    var encryptSuccess, encryptError;
-
     if (!path || arguments.length === 0) return;
-
-    encryptSuccess = onSuccess.bind(null, success);
-    encryptError = onError.bind(null, error);
-
+    
     exec(success, error, PLUGIN_NAME, 'encrypt', [path, password]);
   },
   decrypt: function (path, password, success, error) {
-    var decryptSuccess, decryptError;
-
     if (!path || arguments.length === 0) return;
-
-    decryptSuccess = onSuccess.bind(null, success);
-    decryptError = onError.bind(null, error);
 
     exec(success, error, PLUGIN_NAME, 'decrypt', [path, password]);
   },
